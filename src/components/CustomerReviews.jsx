6df09b1099eb591899d6e7d85fe9e9577583eec3
Santiago PEREZ <137639404+santiperez98@users.noreply.github.com>
2024-10-13 23:11:17 -0300

    modificaciones

--- src/components/CustomerReviews.jsx
+++ src/components/CustomerReviews.jsx
@@ -1,40 +1,40 @@
 import React from 'react';
 
-const reviews = [
+const comentarios = [
   {
-    text: "“Excelente producto, fui atendido por su dueño, persona muy atenta al momento de la visita.”",
-    author: "Ricardo Guzman",
-    image: "https://lh3.googleusercontent.com/a-/ALV-UjXJh_pfKnJpe9vpk0wpI1rIp_neKa0pt75mTbXcrVOn_c-XSk3R=w60-h60-p-rp-mo-ba3-br100",
+    texto: "“Excelente producto, fui atendido por su dueño, persona muy atenta al momento de la visita.”",
+    autor: "Ricardo Guzmán",
+    imagen: "https://lh3.googleusercontent.com/a-/ALV-UjXJh_pfKnJpe9vpk0wpI1rIp_neKa0pt75mTbXcrVOn_c-XSk3R=w60-h60-p-rp-mo-ba3-br100",
   },
   {
-    text: "“Las mejores piedras para afilar naturales provenientes de una de las pocas canteras del mundo.”",
-    author: "José Manuel Seoane",
-    image: "https://lh3.googleusercontent.com/a-/ALV-UjXshJfLyNDAqpIfSTjV1SzHIN0IN7VLRkSmVb7MuIs1_X1VpLBgOQ=w60-h60-p-rp-mo-ba6-br100",
+    texto: "“Las mejores piedras para afilar naturales provenientes de una de las pocas canteras del mundo.”",
+    autor: "José Manuel Seoane",
+    imagen: "https://lh3.googleusercontent.com/a-/ALV-UjXshJfLyNDAqpIfSTjV1SzHIN0IN7VLRkSmVb7MuIs1_X1VpLBgOQ=w60-h60-p-rp-mo-ba6-br100",
   },
   {
-    text: "“Muy buenas piedras para afilar. Debieran tener un distribuidor en Chile.”",
-    author: "Francisco Edwards",
-    image: "https://lh3.googleusercontent.com/a-/ALV-UjW15_SmceW47O15QcRcuR5IM_c3GeuDQFWzyEv6OMbeHRXc0co=w60-h60-p-rp-mo-ba4-br100",
+    texto: "“Muy buenas piedras para afilar. Debieran tener un distribuidor en Chile.”",
+    autor: "Francisco Edwards",
+    imagen: "https://lh3.googleusercontent.com/a-/ALV-UjW15_SmceW47O15QcRcuR5IM_c3GeuDQFWzyEv6OMbeHRXc0co=w60-h60-p-rp-mo-ba4-br100",
   },
   {
-    text: "“Las mejores piedras de afilar sin dudas.”",
-    author: "Javier Gonzalez",
-    image: "https://lh3.googleusercontent.com/a/ACg8ocJjKdTOrkSXDG5bjkgU9MtzL0tLdpEJTGjiKq-egJt-oG5__w=w60-h60-p-rp-mo-ba3-br100",
+    texto: "“Las mejores piedras de afilar sin dudas.”",
+    autor: "Javier González",
+    imagen: "https://lh3.googleusercontent.com/a/ACg8ocJjKdTOrkSXDG5bjkgU9MtzL0tLdpEJTGjiKq-egJt-oG5__w=w60-h60-p-rp-mo-ba3-br100",
   },
   {
-    text: "“Excelentes piedras, excelente la atención, Elvio un maestro 🙏.”",
-    author: "Arturo Gerosa",
-    image: "https://lh3.googleusercontent.com/a/ACg8ocK2sfi9ckj9SOHG_yXKjo1hg89bjefO6ytQ7o-ot8BiPhsS5w=w60-h60-p-rp-mo-br100",
+    texto: "“Excelentes piedras, excelente la atención, Elvio un maestro 🙏.”",
+    autor: "Arturo Gerosa",
+    imagen: "https://lh3.googleusercontent.com/a/ACg8ocK2sfi9ckj9SOHG_yXKjo1hg89bjefO6ytQ7o-ot8BiPhsS5w=w60-h60-p-rp-mo-br100",
   },
   {
-    text: "“Excelente atención.”",
-    author: "Alfredo Luis Lodi",
-    image: "https://lh3.googleusercontent.com/a-/ALV-UjXsArz3J-5pLzzlhFj_F2URU4W4j2VQiZHSbVhqH4F6bflXqVg5Aw=w60-h60-p-rp-mo-ba5-br100",
+    texto: "“Excelente atención.”",
+    autor: "Alfredo Luis Lodi",
+    imagen: "https://lh3.googleusercontent.com/a-/ALV-UjXsArz3J-5pLzzlhFj_F2URU4W4j2VQiZHSbVhqH4F6bflXqVg5Aw=w60-h60-p-rp-mo-ba5-br100",
   },
 ];
 
 // Componente para mostrar estrellas
-const StarRating = () => {
+const CalificacionEstrellas = () => {
   return (
     <div className="flex justify-center mt-2">
       {[...Array(5)].map((_, index) => (
@@ -58,23 +58,23 @@ const StarRating = () => {
   );
 };
 
-const CustomerReviewsCarousel = () => {
+const CarruselComentariosClientes = () => {
   return (
     <div className="relative w-full overflow-hidden bg-gray-100 py-8">
       <h2 className="text-3xl font-bold text-center mb-6 text-gray-800">
         Comentarios de Clientes
       </h2>
       <div className="flex animate-scroll space-x-8">
-        {[...reviews, ...reviews].map((review, index) => (
+        {[...comentarios, ...comentarios].map((comentario, index) => (
           <div key={index} className="flex-shrink-0 w-72 p-4 bg-white rounded-lg shadow-md text-center">
             <img
-              src={review.image}
-              alt={review.author}
+              src={comentario.imagen}
+              alt={comentario.autor}
               className="w-16 h-16 rounded-full mx-auto mb-4"
             />
-            <p className="italic text-gray-700">{review.text}</p>
-            <StarRating /> {/* Componente de estrellas */}
-            <p className="font-semibold text-gray-800 mt-2">{review.author}</p>
+            <p className="italic text-gray-700">{comentario.texto}</p>
+            <CalificacionEstrellas /> {/* Componente de estrellas */}
+            <p className="font-semibold text-gray-800 mt-2">{comentario.autor}</p>
           </div>
         ))}
       </div>
@@ -83,8 +83,8 @@ const CustomerReviewsCarousel = () => {
 };
 
 // Estilos de la animación
-const style = document.createElement('style');
-style.innerHTML = `
+const estilo = document.createElement('style');
+estilo.innerHTML = `
   @keyframes scroll {
     0% { transform: translateX(0); }
     100% { transform: translateX(-50%); }
@@ -95,6 +95,6 @@ style.innerHTML = `
     animation: scroll 20s linear infinite;
   }
 `;
-document.head.appendChild(style);
+document.head.appendChild(estilo);
 
-export default CustomerReviewsCarousel;
+export default CarruselComentariosClientes;
