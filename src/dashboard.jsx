b138cf42bb693b6e6e05bdf8f30a6c3f70b53e90
unknown <Jorgemathez14@gmail.com>
2024-10-14 23:48:13 -0300

    probar deploy

--- src/dashboard.jsx
+++ src/dashboard.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 
-import SimpleSidebar from './components/Sidebar'
+
 import Sidebar from './components/Sidebar'
 
 
 function dashboard() {
   return (
     <div>
-      
     <Sidebar/>
     </div>
   )
